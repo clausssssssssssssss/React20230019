0d5e330377d44348f38eb02ab4c091b0a8f5af72
Claudia <Claudia>
2025-03-18 10:38:18 -0600

    avanze

---
+++ claudia/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Alerta from './components/Alerta';
+
+function App() {
+  return (
+    <>
+    <Alerta/>
+    </>
+  ); 
+
+}
+
+export default App;
